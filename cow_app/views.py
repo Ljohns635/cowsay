3c482b709b2a23429c38c41ccfcf608712d160e0
Latisha Johnson <latishaj966@gmail.com>
2021-02-11 16:51:02 -0800

    got cow to show up on the page

--- cow_app/views.py
+++ cow_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from cow_app.models import CowText
 from cow_app.forms import CowTextForm
 import subprocess
+from subprocess import PIPE
+
 
 def index_view(request):
     context = {}
@@ -10,10 +12,13 @@ def index_view(request):
         if form.is_valid():
             data = form.cleaned_data
             CowText.objects.create(
-                speak = data['speak']
+                speak = data['speak'],
             )
-            return HttpResponseRedirect(request.path_info)
-           
+            speak = subprocess.check_output(['cowsay', data.get('speak')])
+            speak = speak.decode('utf-8')
+            form = CowTextForm()
+            return render(request, 'index.html', {'speak': speak, 'form': form})
+
     form = CowTextForm()
     context.update({'form': form})
     return render(request, 'index.html', context)
